6d93f787400e56fd6bc633e8faa46e9efb077e4f
Ariel Ogdowski <a.ogdowski@datacapt.com>
2026-01-30 16:04:12 +0100

    Release v0.5.1

--- backend/app/config.py
+++ backend/app/config.py
@@ -29,7 +29,7 @@ class Settings(BaseSettings):
     DISCORD_CLIENT_SECRET: Optional[str] = None
 
     # Application
-    SQUIG_VERSION: str = "0.5.0"
+    SQUIG_VERSION: str = "0.5.1"
     APP_NAME: str = "SquigLeague"
 
     # Admin
